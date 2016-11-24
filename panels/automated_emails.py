23b0d357b1ccdd4796235ab7e09dbc9b4499720e
Eli Courtwright <eli@courtwright.org>
2016-11-24 00:37:43 -0500

    fixes #23

--- panels/automated_emails.py
+++ panels/automated_emails.py
@@ -9,20 +9,24 @@ class PanelAppEmail(AutomatedEmail):
             kwargs['filter'] = lambda x: True
         AutomatedEmail.__init__(self, PanelApplication, *args, sender=c.PANELS_EMAIL, **kwargs)
 
-PanelAppEmail('Your {EVENT_NAME} Panel Application Has Been Received', 'panel_app_confirmation.txt')
+    def computed_subject(self, x):
+        return self.subject.replace('<PANEL_NAME>', x.name)
 
-PanelAppEmail('Your {EVENT_NAME} Panel Application Has Been Accepted', 'panel_app_accepted.txt',
+
+PanelAppEmail('Your {EVENT_NAME} Panel Application Has Been Received: <PANEL_NAME>', 'panel_app_confirmation.txt')
+
+PanelAppEmail('Your {EVENT_NAME} Panel Application Has Been Accepted: <PANEL_NAME>', 'panel_app_accepted.txt',
               lambda app: app.status == c.ACCEPTED,
               ident='panel_accepted')
 
-PanelAppEmail('Your {EVENT_NAME} Panel Application Has Been Declined', 'panel_app_declined.txt',
+PanelAppEmail('Your {EVENT_NAME} Panel Application Has Been Declined: <PANEL_NAME>', 'panel_app_declined.txt',
               lambda app: app.status == c.DECLINED,
               ident='panel_declined')
 
-PanelAppEmail('Your {EVENT_NAME} Panel Application Has Been Waitlisted', 'panel_app_waitlisted.txt',
+PanelAppEmail('Your {EVENT_NAME} Panel Application Has Been Waitlisted: <PANEL_NAME>', 'panel_app_waitlisted.txt',
               lambda app: app.status == c.WAITLISTED,
               ident='panel_waitlisted')
 
-PanelAppEmail('Your {EVENT_NAME} Panel Has Been Scheduled', 'panel_app_scheduled.txt',
+PanelAppEmail('Your {EVENT_NAME} Panel Has Been Scheduled: <PANEL_NAME>', 'panel_app_scheduled.txt',
               lambda app: app.event_id,
               ident='panel_scheduled')
