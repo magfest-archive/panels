abccede8c5f7bd59e28242121c57381355d2fe27
Victoria Earl <kitsuta@gmail.com>
2017-09-27 14:49:22 -0400

    Customize panel application for guests Add an 'available' column so we can ask guests their for availability, not unavailability. Also removes the sixty minute checks, and allows a "return_to" parameter so guests can go back to their checklist when they're done.

---
+++ alembic/versions/2f5f9d87b056_add_available_column.py
@@ -0,0 +1,63 @@
+"""Add available column
+
+Revision ID: 2f5f9d87b056
+Revises: 416eb615ff1a
+Create Date: 2017-09-27 14:05:52.746446
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = '2f5f9d87b056'
+down_revision = '416eb615ff1a'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+try:
+    is_sqlite = op.get_context().dialect.name == 'sqlite'
+except:
+    is_sqlite = False
+
+if is_sqlite:
+    op.get_context().connection.execute('PRAGMA foreign_keys=ON;')
+    utcnow_server_default = "(datetime('now', 'utc'))"
+else:
+    utcnow_server_default = "timezone('utc', current_timestamp)"
+
+def sqlite_column_reflect_listener(inspector, table, column_info):
+    """Adds parenthesis around SQLite datetime defaults for utcnow."""
+    if column_info['default'] == "datetime('now', 'utc')":
+        column_info['default'] = utcnow_server_default
+
+sqlite_reflect_kwargs = {
+    'listeners': [('column_reflect', sqlite_column_reflect_listener)]
+}
+
+# ===========================================================================
+# HOWTO: Handle alter statements in SQLite
+#
+# def upgrade():
+#     if is_sqlite:
+#         with op.batch_alter_table('table_name', reflect_kwargs=sqlite_reflect_kwargs) as batch_op:
+#             batch_op.alter_column('column_name', type_=sa.Unicode(), server_default='', nullable=False)
+#     else:
+#         op.alter_column('table_name', 'column_name', type_=sa.Unicode(), server_default='', nullable=False)
+#
+# ===========================================================================
+
+
+def upgrade():
+    if is_sqlite:
+        with op.batch_alter_table('panel_application', reflect_kwargs=sqlite_reflect_kwargs) as batch_op:
+            batch_op.add_column(sa.Column('available', sa.Unicode(), server_default='', nullable=False))
+    else:
+        op.add_column('panel_application', sa.Column('available', sa.Unicode(), server_default='', nullable=False))
+
+
+def downgrade():
+    op.drop_column('panel_application', 'available')
