2f7e45882ae00a07bca981fc2c2f0566f5bf94bb
Rob Ruana <rob@robruana.com>
2017-09-30 12:53:28 -0400

    Cleans up schedule layout

--- panels/site_sections/schedule.py
+++ panels/site_sections/schedule.py
@@ -11,7 +11,6 @@ class Root:
         else:
             raise HTTPRedirect("internal")
 
-    @cached
     def internal(self, session, message=''):
         if c.HIDE_SCHEDULE and not AdminAccount.access_set() and not cherrypy.session.get('staffer_id'):
             return "The " + c.EVENT_NAME + " schedule is being developed and will be made public when it's closer to being finalized."
