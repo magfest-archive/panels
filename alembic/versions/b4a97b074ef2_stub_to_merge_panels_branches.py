10acb698868dbacac42036d25be681988063a884
Rob Ruana <rob@robruana.com>
2017-10-18 20:47:14 -0400

    Adds migration stub to merge erroneous alembic branches

---
+++ alembic/versions/b4a97b074ef2_stub_to_merge_panels_branches.py
@@ -0,0 +1,22 @@
+"""Stub to merge panels branches and rewrite the branch history
+
+Revision ID: b4a97b074ef2
+Revises: 2f5f9d87b056, 3733faf640e9, a3d71270256c
+Create Date: 2017-10-18 20:32:15.873425
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4a97b074ef2'
+down_revision = ('2f5f9d87b056', '3733faf640e9', 'a3d71270256c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
